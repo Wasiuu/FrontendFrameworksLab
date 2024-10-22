ee5eec178bac4f1dee87a835657e13bba4788db2
Kuba <wasylik109@gmail.com>
2024-10-22 12:32:47 +0200

    car update

--- lab01/src/module-data.js
+++ lab01/src/module-data.js
@@ -2,61 +2,61 @@ export const cars = [
   {
     id: 1,
     brand: "Honda",
-    registrationNumber: "TV-310",
-    year: 2017
+    registrationNumber: "LF-773",
+    year: 2002
   },
   {
     id: 2,
-    brand: "Mercedes",
-    registrationNumber: "BH-045",
-    year: 2009
-  },
-  {
-    id: 3,
-    brand: "BMW",
-    registrationNumber: "NJ-135",
-    year: 2018
-  },
-  {
-    id: 4,
-    brand: "Ford",
-    registrationNumber: "MS-481",
-    year: 2001
-  },
-  {
-    id: 5,
-    brand: "Honda",
-    registrationNumber: "NY-434",
-    year: 2009
-  },
-  {
-    id: 6,
-    brand: "Audi",
-    registrationNumber: "KD-698",
-    year: 2005
-  },
-  {
-    id: 7,
-    brand: "BMW",
-    registrationNumber: "FY-234",
-    year: 2003
-  },
-  {
-    id: 8,
-    brand: "Mercedes",
-    registrationNumber: "BT-991",
-    year: 2005
-  },
-  {
-    id: 9,
     brand: "Toyota",
-    registrationNumber: "IM-968",
+    registrationNumber: "WR-046",
     year: 2010
   },
   {
-    id: 10,
+    id: 3,
+    brand: "Mercedes",
+    registrationNumber: "CY-738",
+    year: 2005
+  },
+  {
+    id: 4,
     brand: "Toyota",
-    registrationNumber: "JO-906",
-    year: 2009
+    registrationNumber: "ZI-264",
+    year: 2016
+  },
+  {
+    id: 5,
+    brand: "BMW",
+    registrationNumber: "AX-287",
+    year: 2024
+  },
+  {
+    id: 6,
+    brand: "Mercedes",
+    registrationNumber: "FC-655",
+    year: 2023
+  },
+  {
+    id: 7,
+    brand: "Ford",
+    registrationNumber: "AP-679",
+    year: 2020
+  },
+  {
+    id: 8,
+    brand: "Toyota",
+    registrationNumber: "KV-300",
+    year: 2000
+  },
+  {
+    id: 9,
+    brand: "Ford",
+    registrationNumber: "VT-966",
+    year: 2001
+  },
+  {
+    id: 10,
+    brand: "Audi",
+    registrationNumber: "OS-208",
+    year: 2000
   },
 ];
